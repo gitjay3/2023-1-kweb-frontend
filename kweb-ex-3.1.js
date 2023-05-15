27fcfb11d4df84e34c59c28d4ae4cf2b41f01c41
gitjay3 <129665823+gitjay3@users.noreply.github.com>
2023-05-15 16:16:59 +0900

    Delete kweb-ex-3.1.js

--- kweb-ex-3.1.js
+++
@@ -1,7 +0,0 @@
-function isValidNumber(x){
-    const result = parseInt(x);
-    if(!isFinite(result)) return false;
-    if(result != x) return false;
-    if(result>=1 && result<=9) return true;
-    else return false;
-};
